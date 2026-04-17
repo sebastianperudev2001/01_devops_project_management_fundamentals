5e2a40e48999190818c2b5e22e488df1ca715369
Sebastian Chavarry <schavarry01@gmail.com>
2026-04-16 19:15:24 -0500

    feat: added env

--- test.sh
+++ test.sh
@@ -7,6 +7,8 @@ echo "=========================================="
 
 # Verificar versión de bash
 echo "✅ Bash version: $BASH_VERSION"
+echo "Entorno: $APP_ENV"
+
 
 # Verificar que un archivo existe
 if [ -f "README.md" ]; then
